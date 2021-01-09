c2094f80c684bef327c076b1b3bb309ba1d027a8
Justus Böhme <j.d.boehme123@gmail.com>
2021-01-09 17:50:25 +0100

    Added auto release

--- package.ps1
+++ package.ps1
@@ -1,4 +1,6 @@
 rm -r out
+$ghCurrent = $(gh config get prompt)
+gh config set prompt disabled
 electron-forge package --arch=x64 --platform=linux
 electron-forge package --arch=arm64 --platform=linux
 electron-forge package --arch=armv7l --platform=linux
@@ -17,3 +19,5 @@ compress-archive -path out\musicman-linux-armv7l
 compress-archive -path out\musicman-win32-arm64
 compress-archive -path out\musicman-win32-x64
 compress-archive -path out\musicman-win32-ia32
+gh release create v0-unversioned out\musicman-linux-x64.zip out\musicman-linux-arm64.zip out\musicman-linux-armv7l.zip out\musicman-win32-x64.zip out\musicman-win32-arm64.zip out\musicman-win32-ia32.zip -d
+gh config set prompt $ghCurrent
