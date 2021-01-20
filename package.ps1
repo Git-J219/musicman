3d2cf2bcc530d15cf6915c7855bd12d6c579c781
Justus Böhme <j.d.boehme123@gmail.com>
2021-01-20 10:34:23 +0100

    Added log output

--- package.ps1
+++ package.ps1
@@ -1,23 +1,43 @@
-rm -r out
+$null = rm -r out
+echo "Der Ausgabeordner wurde gelöscht"
 $ghCurrent = $(gh config get prompt)
-gh config set prompt disabled
-electron-forge package --arch=x64 --platform=linux
-electron-forge package --arch=arm64 --platform=linux
-electron-forge package --arch=armv7l --platform=linux
-electron-forge package --arch=arm64 --platform=win32
-electron-forge package --arch=x64 --platform=win32
-electron-forge package --arch=ia32 --platform=win32
-cp 3rdParty out\musicman-linux-x64 -r
-cp 3rdParty out\musicman-linux-arm64 -r
-cp 3rdParty out\musicman-linux-armv7l -r
-cp 3rdParty out\musicman-win32-arm64 -r
-cp 3rdParty out\musicman-win32-x64 -r
-cp 3rdParty out\musicman-win32-ia32 -r
-compress-archive -path out\musicman-linux-x64 -destinationpath out\musicman-linux-x64.zip
-compress-archive -path out\musicman-linux-arm64 -destinationpath out\musicman-linux-arm64.zip
-compress-archive -path out\musicman-linux-armv7l -destinationpath out\musicman-linux-armv7l.zip
-compress-archive -path out\musicman-win32-arm64 -destinationpath out\musicman-win32-arm64.zip
-compress-archive -path out\musicman-win32-x64 -destinationpath out\musicman-win32-x64.zip
-compress-archive -path out\musicman-win32-ia32 -destinationpath out\musicman-win32-ia32.zip
-gh release create vunlabeled out\musicman-linux-x64.zip out\musicman-linux-arm64.zip out\musicman-linux-armv7l.zip out\musicman-win32-x64.zip out\musicman-win32-arm64.zip out\musicman-win32-ia32.zip -d
+$null = gh config set prompt disabled
+echo "Verpacken: linux x64"
+$null = electron-forge package --arch=x64 --platform=linux
+echo "Verpacken: linux arm64"
+$null = electron-forge package --arch=arm64 --platform=linux
+echo "Verpacken: linux armv7l"
+$null = electron-forge package --arch=armv7l --platform=linux
+echo "Verpacken: win32 arm64"
+$null = electron-forge package --arch=arm64 --platform=win32
+echo "Verpacken: win32 x64"
+$null = electron-forge package --arch=x64 --platform=win32
+echo "Verpacken: win32 ia32"
+$null = electron-forge package --arch=ia32 --platform=win32
+echo "Lizensen kopieren: linux x64"
+$null = cp 3rdParty out\musicman-linux-x64 -r
+echo "Lizensen kopieren: linux arm64"
+$null = cp 3rdParty out\musicman-linux-arm64 -r
+echo "Lizensen kopieren: linux armv7l"
+$null = cp 3rdParty out\musicman-linux-armv7l -r
+echo "Lizensen kopieren: win32 arm64"
+$null = cp 3rdParty out\musicman-win32-arm64 -r
+echo "Lizensen kopieren: win32 x64"
+$null = cp 3rdParty out\musicman-win32-x64 -r
+echo "Lizensen kopieren: win32 ia32"
+$null = cp 3rdParty out\musicman-win32-ia32 -r
+echo "ZIP: linux x64"
+$null = compress-archive -path out\musicman-linux-x64 -destinationpath out\musicman-linux-x64.zip
+echo "ZIP: linux arm64"
+$null = compress-archive -path out\musicman-linux-arm64 -destinationpath out\musicman-linux-arm64.zip
+echo "ZIP: linux armv7l"
+$null = compress-archive -path out\musicman-linux-armv7l -destinationpath out\musicman-linux-armv7l.zip
+echo "ZIP: win32 arm64"
+$null = compress-archive -path out\musicman-win32-arm64 -destinationpath out\musicman-win32-arm64.zip
+echo "ZIP: win32 x64"
+$null = compress-archive -path out\musicman-win32-x64 -destinationpath out\musicman-win32-x64.zip
+echo "ZIP: win32 ia32"
+$null = compress-archive -path out\musicman-win32-ia32 -destinationpath out\musicman-win32-ia32.zip
+echo "Hochladen..."
+$null = gh release create vunlabeled out\musicman-linux-x64.zip out\musicman-linux-arm64.zip out\musicman-linux-armv7l.zip out\musicman-win32-x64.zip out\musicman-win32-arm64.zip out\musicman-win32-ia32.zip -d
 gh config set prompt $ghCurrent
