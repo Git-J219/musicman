c38e6f5060f99864e3e300ccae6260fdbe6e2411
Git-J219 <64012180+Git-J219@users.noreply.github.com>
2023-11-02 14:50:20 +0100

    Remove long delay at startup

--- src/inter.js
+++ src/inter.js
@@ -432,4 +432,4 @@ document.querySelector('#updateDisable').addEventListener('click', () => {
 document.querySelector('#app_max').addEventListener('click', windowControl.maximize);
 document.querySelector('#app_min').addEventListener('click', windowControl.minimize);
 document.querySelector('#app_close').addEventListener('click', windowControl.close);
-window.setTimeout(init.completed, 1000);
+window.setTimeout(init.completed, 5);
